2762170e0736a6c83baac71a9f1e142fe83b261a
Zurc <cruzjurado@gmail.com>
2018-11-07 08:46:48 +0000

    Update app.js

--- app.js
+++ app.js
@@ -4,4 +4,4 @@ setInterval(() => {
 
   opn('https://www.youtube.com/watch?v=2nGKqH26xlg');
 
-}, 60 * 60 * 1000)  // break every hour
+}, 60 * 60 * 1000)  // take a break every hour, walk around. Your body will appreciate this ;)
